009a53142a24d6f1ec451730d4adfb8e34161b0f
omar2004712 <o.bahutar@gmail.com>
2022-07-29 09:08:21 +0400

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const cookieSession = require('cookie-session');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const mongoose = require("mongoose");
 
-const authRouter = require('./routes/admin/auth')
-const adminProductsRouter = require('./routes/admin/products')
-const productsRouter = require('./routes/products');
-const cartRouter = require('./routes/carts')
+const authRouter = require("./routes/admin/auth");
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
+const cartRouter = require("./routes/carts");
+const User = require("./model/user");
+const Cart = require("./model/cart");
+const Product = require("./model/product");
+
+mongoose.connect("mongodb://localhost/ecomm");
 
 const app = express();
 
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(cookieSession({
-    keys: ['flutbrtmiaptiwetnWTpia058a420q34vnp4v8[2-qb9-3v9n-0w385vb3qc80']
-}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["flutbrtmiaptiwetnWTpia058a420q34vnp4v8[2-qb9-3v9n-0w385vb3qc80"],
+  })
+);
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
-app.use(cartRouter)
+app.use(cartRouter);
 
-app.listen(3000, ()=>{
-    console.log('Listening')
+app.listen(3000, () => {
+  console.log("Listening");
 });
